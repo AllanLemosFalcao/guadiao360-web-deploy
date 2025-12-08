42e52140e5c139537c0c9739d9b7450dbd863581
AllanLemosFalcao <allanlmfalcao@gmail.com>
2025-12-08 13:57:56 -0300

    pwa

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
-// Importe seu arquivo de estilos globais
 import './styles/global.css' 
+
+// --- IMPORTAÇÃO DO PWA ---
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("Nova atualização disponível. Recarregar?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('App pronto para uso offline.');
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
